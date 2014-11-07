38c3a883331dac9e294f4b10ef25086c21546aaf
Matt Coffin <mcoffin13@gmail.com>
2014-11-07 12:26:02 -0800

    Fix help message sending

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 function help(command, source, target) {
-    irc("PRIVMSG " + source + " :I don't really serve a purpose right now.");
+    irc("PRIVMSG " + target + " :I don't really serve a purpose right now.");
 }
